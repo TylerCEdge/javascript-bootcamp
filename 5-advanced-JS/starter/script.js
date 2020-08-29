2ee4bdc061baac62952536bf6ba1fa947e4a262c
TylerCEdge <tylercedge@gmail.com>
2020-08-29 14:48:17 -0400

    worked through some prototyping examples as well as some demos showing primatives vs objects vs functions.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -7,23 +7,78 @@
 // };
 
 // Function constructor
-var Person = function (name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-};
+// var Person = function (name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// };
 
 // This works because of inheritence
-Person.prototype.calculateAge = function () {
-  console.log(2020 - this.yearOfBirth);
+// Person.prototype.calculateAge = function () {
+//   console.log(2020 - this.yearOfBirth);
+// };
+
+// Person.prototype.lastName = 'Smith';
+
+// var john = new Person('John', 1990, 'teacher');
+// var jane = new Person('Jane', 1969, 'designer');
+// var mark = new Person('Mark', 1948, 'retired');
+
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+
+// var personProto = {
+//   calculateAge: function () {
+//     console.log(2020 - this.yearOfBirth);
+//   },
+// };
+
+// var john = Object.create(personProto);
+// john.name = 'John';
+// john.yearOfBirth = 1990;
+// john.job = 'teacher';
+// console.log(john);
+
+// var jane = Object.create(personProto, {
+//   name: { value: 'jane' },
+//   yearOfBirth: { value: 1969 },
+//   job: { value: 'designer' },
+// });
+// console.log(jane);
+
+// Primatives vs Objects
+
+// Primatives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a, b);
+
+// Objects
+var obj1 = {
+  name: 'john',
+  age: 26,
 };
 
-Person.prototype.lastName = 'Smith';
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
 
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
+// Functions
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon',
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = 'San Francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);

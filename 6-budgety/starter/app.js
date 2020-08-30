bb87ad0db29adbd12354daf9a9e7f9bad2638388
TylerCEdge <tylercedge@gmail.com>
2020-08-30 19:41:48 -0400

    working on an add item method currently working through how to add a unique id

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,27 +1,92 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
-  var x = 23;
-
-  var add = function (a) {
-    return x + a;
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
   };
 
   return {
-    publicTest: function (b) {
-      return add(b);
+    addItem: function (type, des, val) {
+      var newItem, ID;
+      ID = 0;
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+  };
+})();
+// UI CONTROLLER
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+  return {
+    getinput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
     },
   };
 })();
 
-var UIController = (function () {
-  // Some Code
-})();
-
+// GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+  var setEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', (e) => {
+      if (e.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function () {
+    // 1. Get input data.
+    var input = UICtrl.getinput();
+    // 2. Add item to budget controller.
+    // 3. Add item to the User Interface.
+    // 4. Calculate the budget.
+    // 5. Display the budget.
+  };
 
   return {
-    anotherPublic: function () {
-      console.log(z);
+    init: function () {
+      console.log('Application has started ...');
+      setEventListeners();
     },
   };
 })(budgetController, UIController);
+
+controller.init();

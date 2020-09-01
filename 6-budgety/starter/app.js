6a5a4d0aedb7274010ef8ad3d734f11c0776ad5b
TylerCEdge <tylercedge@gmail.com>
2020-09-01 14:31:53 -0400

    modified input fields, plus and minus toggler and submit button to toggle from blue to red blue (inc) red (exp).

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -162,6 +162,12 @@ var UIController = (function () {
     return (type === 'exp' ? '-' : '+') + ' ' + '$' + int + '.' + dec;
   };
 
+  var nodeListForEach = function (list, cb) {
+    for (var i = 0; i < list.length; i++) {
+      cb(list[i], i);
+    }
+  };
+
   return {
     getinput: function () {
       return {
@@ -243,12 +249,6 @@ var UIController = (function () {
 
       fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);
 
-      nodeListForEach = function (list, cb) {
-        for (var i = 0; i < list.length; i++) {
-          cb(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function (current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -282,6 +282,24 @@ var UIController = (function () {
         months[month] + ' ' + year;
     },
 
+    changedType: function () {
+      var fields;
+
+      fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ',' +
+          DOMstrings.inputDescription +
+          ',' +
+          DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function (cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -304,6 +322,10 @@ var controller = (function (budgetCtrl, UICtrl) {
     document
       .querySelector(DOM.container)
       .addEventListener('click', ctrlDeleteItem);
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener('change', UICtrl.changedType);
   };
 
   var updateBudget = function () {
